066c4d3aaef3ea34aeb67b6d0c25f8b4805aa1a8
Pieter K <51260315+SuperPieter@users.noreply.github.com>
2020-05-04 18:21:00 +0200

    Add files via upload

---
+++ Better COVID-19/script.js
@@ -0,0 +1,13 @@
+replaceTextCovid(document.body);
+function replaceTextCovid(element) {
+    if (element.hasChildNodes()) {
+      element.childNodes.forEach(replaceTextCovid)
+    } else if (element.nodeType === Text.TEXT_NODE){
+      const re = new RegExp("covid-19", "i");
+      if(element.textContent.match(re)) {
+        const newElement = document.createElement('span');
+        newElement.innerHTML = element.textContent.replace(re, '<span class="SuperGRB"><b>COVID-19 🤢</b></span>')
+        element.replaceWith(newElement)
+    }
+  }
+}
